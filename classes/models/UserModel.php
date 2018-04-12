f0c0859b285a003b109f8e3ae23c6f518f2f704d
Alexey Dzyuba <alexeydzyuba27@gmail.com>
2018-04-12 11:32:22 +0300

    eerr

--- classes/models/UserModel.php
+++ classes/models/UserModel.php
@@ -32,6 +32,7 @@ class UserModel extends BaseModel {
 	public function createEmptyUserTodos() {
 		$query = "INSERT INTO todos_lists (user_id) VALUES (:userId)";
 		$userSessId = self::getUserSessId();
+		var_dump($userSessId);
 
 		$payload = $this->db->prepare($query);
 		$payload->bindParam(':userId', $userSessId, \PDO::PARAM_INT);
