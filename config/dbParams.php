9bd8a4a95d900d165cdd1f57ee8a4701008c24a4
Alexey Dzyuba <alexeydzyuba27@gmail.com>
2018-04-12 10:05:57 +0300

    her-final

--- config/dbParams.php
+++ config/dbParams.php
@@ -1,13 +1,13 @@
 <?php
 
 return [
-	'host' => 'localhost',
-	'dbname' => 'mytodos.os',
-	'username' => 'root',
-	'password' => ''
+//	'host' => 'localhost',
+//	'dbname' => 'mytodos.os',
+//	'username' => 'root',
+//	'password' => ''
 // HEROKU CREDENTIALS FOR DB
-//	'host' => 'us-cdbr-iron-east-05.cleardb.net',
-//	'dbname' => 'heroku_104490174000225',
-//	'username' => 'bac6640bac80a3',
-//	'password' => 'c7645cd9'
+	'host' => 'us-cdbr-iron-east-05.cleardb.net',
+	'dbname' => 'heroku_104490174000225',
+	'username' => 'bac6640bac80a3',
+	'password' => 'c7645cd9'
 ];
